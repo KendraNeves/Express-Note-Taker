288d22776ba3e5485f14d997dc292ec9b48660e3
kneves <55398273+kneves1@users.noreply.github.com>
2020-04-13 09:44:51 -0400

    Server runs, /notes not running properly

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 let path = require("path");
 let router = require("express").Router();
 
-router.get("/notes", function(request, response) {
+router.get("notes", function(request, response) {
     // send  notes file
     response.sendFile(path.join(__dirname, "../public/notes.html"));
 });
--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,17 @@
+var express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 // basic server template
 // see 14-FinalStarwarsApp/server-template.js
 // plus require routes 
+var app = express();
+var PORT = process.env.PORT || 8080;
 
 // PLUS the following 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 /* 
 ^^ creates a path to static front-end files: html, css & js.
 notice how files are referenced in index.html: 
@@ -14,3 +22,6 @@ to the /public directory automatically.
 app.use("/api", apiRoutes);  // /api/notes
 app.use("/", htmlRoutes);    // /notes or /
 
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
